0ca6d23ebcfde5ebd719f2f307e8689fc8931557
BK Pecho <bkpecho05@gmail.com>
2022-11-30 13:35:22 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Landingpage/>
+  </div>
+
+</template>
+
+<script>
+
+import Landingpage from './components/Landingpage.vue'
+
+export default{
+  name:'App',
+  components: {
+    Landingpage,
+  },
+};
+
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+
+import './assets/main.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
+createApp(App).mount('#app')
